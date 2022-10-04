dac28e67e88a239e4a6f5501c1f432d090db380a
Andrii-Vekker <65786577+Andrii-Vekker@users.noreply.github.com>
2022-10-04 19:14:07 +0300

    start

---
+++ src/components/Api/Api.jsx
@@ -0,0 +1,23 @@
+
+
+const key = "8d439eb5ac7a153643a933bcb130103b"
+
+const getTranding = () => {
+
+}
+
+const getSearch = () => { 
+
+}
+
+const getDetails = () => {
+
+ }
+
+const getActorDetails = () => {
+
+ }
+
+const getRewiev = () => {
+
+ }
---
+++ src/components/App/App.jsx
@@ -0,0 +1,21 @@
+import Cast from "pages/Cast/Cast";
+import Home from "pages/Home/Home";
+import MovieDetails from "pages/MovieDetails/MovieDetails";
+import Movies from "pages/Movies/Movies";
+import Reviews from "pages/Reviews/Reviews";
+import { Routes, Route } from "react-router-dom";
+
+
+export const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/movies" element={<Movies/>} />
+        <Route path="/movies/:movieId" element={<MovieDetails/>} />
+        <Route path="/movies/:movieId/cast" element={<Cast/>} />
+        <Route path="/movies/:movieId/reviews" element={<Reviews/>} />
+</Routes>
+    </div>
+  );
+};
