01005791ed2d397b0a0f4919db3a7f1cfcd91b0c
Andrii-Vekker <65786577+Andrii-Vekker@users.noreply.github.com>
2022-10-05 07:41:29 +0300

    weqw

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,14 +8,14 @@ import { Routes, Route } from "react-router-dom";
 
 export const App = () => {
   return (
-    <div>
+   
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/movies" element={<Movies/>} />
-        <Route path="/movies/:movieId" element={<MovieDetails/>} />
-        <Route path="/movies/:movieId/cast" element={<Cast/>} />
-        <Route path="/movies/:movieId/reviews" element={<Reviews/>} />
+        {/* <Route path="/movies" element={<Movies/>} /> */}
+        {/* <Route path="/movies/:movieId" element={<MovieDetails/>} /> */}
+        {/* <Route path="/movies/:movieId/cast" element={<Cast/>} /> */}
+        {/* <Route path="/movies/:movieId/reviews" element={<Reviews/>} /> */}
 </Routes>
-    </div>
+    
   );
 };
